bb9a6ab5c54fcd351d68ddb6838460223b0fdac4
iradsanchez <ira.d.sanchez@gmail.com>
2019-04-09 20:13:31 -0700

    Add Server

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
 const bcrypt = require("bcrypt");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const port = process.env.port || 4000;
+
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
